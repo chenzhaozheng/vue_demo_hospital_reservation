81a25767f2fa4a5ecc31c6d50dde9c00ceaaa117
chenzhaozheng <389428742@qq.com>
2020-08-01 14:14:16 +0800

    test: 测试+

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vue_demo_hospital_reservation/',
 
     /**
      * Source Maps
