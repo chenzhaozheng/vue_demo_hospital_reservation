50c5b7b6a9e8f6eef4ed0ecc3f76bb89fac6d77e
chenzhaozheng <389428742@qq.com>
2020-08-01 14:10:43 +0800

    test: 测试

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vue_demo_hospital_reservation/',
     proxyTable: {},
 
     // Various Dev Server settings
